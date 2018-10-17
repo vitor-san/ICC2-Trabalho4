876f372f6de597ad15b30b34a235a4ce57f881f2
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2018-10-17 14:02:49 -0300

    Comecei a fazer a arvore binaria (parei na metade por conta da reuniao do Ganesh).

--- btree.c
+++ btree.c
@@ -1,3 +1,71 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "btree.h"
 
+union x {
+	double nbr;
+	char opr;
+};
+
+struct node {
+	Elem info;
+	int isLeaf;
+	Node left;
+	Node right;
+};
+
+Node newNode () {
+	Node new = malloc(sizeof(NODE));
+	new->info.nbr = 0;
+	new->left = NULL;
+	new->right = NULL;
+	return new;
+}
+
+void delNode (Node x) {
+	if (x == NULL) return;
+	
+	if (x->left == NULL && x->right == NULL) {
+		free(x);
+		return;
+	}
+	
+	delNode(x->left);
+	delNode(x->right);
+	free(x);
+
+	return;
+}
+
+Tree newTree () {
+	return newNode();
+}
+
+void insertOper(Tree, char) {
+
+	return;
+}
+
+void insertNumber(Tree, double) {
+
+
+	return;
+}
+
+int isLeaf(Node) {
+
+
+
+
+}
+
+
+void delTree (Tree t) {
+	if (t == NULL) return;
+
+	delTree(t->left);
+	delTree(t->right);
+	delNode(t);
+
+	return;
+}
--- btree.h
+++ btree.h
@@ -1,12 +1,15 @@
 #ifndef BTREE_H
 	#define BTREE_H
 	
-	typedef struct tnode TNODE;
-	typedef TNODE* tNode;
-	typedef struct btree TREE;
-	typedef TREE* Tree;
+	typedef union x Elem;
+	typedef struct node NODE;
+	typedef NODE* Node;
+	typedef Node Tree;
 
 	Tree newTree();
+	void insertOper(Tree, char);
+	void insertNumber(Tree, double);
+	int isLeaf(Node);
 	void delTree(Tree);
 
 #endif
